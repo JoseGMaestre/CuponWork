62d5e12cad3530234589f0806de5bd8be9cd1dd3
Jose Maestre <jose.maestre@linio.com>
2016-04-01 17:04:11 -0430

    Oferta del dia

--- src/Cupon/OfertaBundle/Controller/DefaultController.php
+++ src/Cupon/OfertaBundle/Controller/DefaultController.php
@@ -43,21 +43,14 @@ class DefaultController extends Controller {
 	 */
 	public function ofertaAction($ciudad, $slug) {
 		$em = $this->getDoctrine()->getManager();
-
-		$oferta = $em->getRepository('OfertaBundle:Oferta')->findOferta($ciudad, $slug);
-		$cercanas = $em->getRepository('OfertaBundle:Oferta')->findCercanas($ciudad);
-
-		if (!$oferta) {
-			throw $this->createNotFoundException('No se ha encontrado la oferta solicitada');
-		}
-
-		$respuesta = $this->render('OfertaBundle:Default:detalle.html.twig', array(
-			'cercanas' => $cercanas,
+		$oferta = $em->getRepository('OfertaBundle:Oferta')
+			->findOferta($ciudad, $slug);
+		$relacionadas = $em->getRepository('OfertaBundle:Oferta')
+			->findRelacionadas($ciudad);
+		return $this->render('OfertaBundle:Default:detalle.html.twig', array(
 			'oferta' => $oferta,
+			'relacionadas' => $relacionadas,
 		));
-
-		$respuesta->setSharedMaxAge(60);
-
-		return $respuesta;
 	}
+
 }
